34d217659f3696d58e1b1f39d728632e3ccdfbdf
Olufemi Odunlami <femi9naija@gmail.com>
2023-11-17 19:18:14 +0100

    3. Form Submission Handling

--- html_js_validation/3-form-submission.js
+++ html_js_validation/3-form-submission.js
@@ -3,63 +3,15 @@ const nameEl = document.getElementById('name');
 const emailEl = document.getElementById('email');
 
 //form submission event listener
-form.addEventListener('submit', handleFormSubmit);
 
-//check if email is valid
-const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-};
-
-//error message to display
-const showError = () => {
-    const error = document.getElementById('error');
-    error.textContent = 'Please fill in all required fields'
-}
-
-//success message if all checks passed
-const showSuccess = () => {
-    const success = document.getElementById('success');
-    success.textContent = 'Form submitted successfully!';
-}
-
-//check if required fields are filled
-const isRequired = (value) => {
-    if (value === '') {
-        return false
-    } else {
-        return true;
-    }
-}
-
-//check if name field is filled
-const checkName = () => {
-    let valid = false;
-
-    if (!isRequired(nameEl.value)) {
-        showError();
-    } else {
-        showSuccess();
-        valid = true;
-    }
-    return valid;
-};
-
-//check if email field is filled
-const checkEmail = () => {
-    let valid = false;
-
-    if(!isRequired(emailEl.value)) {
-        showError();
-    } else {
-        showSuccess();
-        valid = true;
-    }
-    return valid;
-}
-
-
-//
 const handleFormSubmit = (event) => {
-    event.prevent
+    event.preventDefault();
+
+    if ((nameEl.value === '') || (emailEl.value === '')) {
+        alert('Please fill in all required fields');
+    } else {
+        alert('Form submitted successfully!');
+    }
 }
+
+form.addEventListener('submit', handleFormSubmit);
